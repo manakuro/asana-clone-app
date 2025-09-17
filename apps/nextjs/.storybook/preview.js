08985602ffdc63a77ec1b4f1cf31bdf55531f854
manato kuroda <a7108100@gmail.com>
2025-09-17 11:26:41 +0900

    refactor: restructure Storybook preview configuration
    Refactored `preview.js` to simplify and modernize Storybook configuration. Replaced legacy exports with a default export, updated the `msw` initialization process, and streamlined decorators for clarity and maintainability. This aligns with current best practices in Storybook configuration.

--- apps/nextjs/.storybook/preview.js
+++ apps/nextjs/.storybook/preview.js
@@ -1,45 +1,47 @@
 import mockdate from 'mockdate';
-import * as msw from 'msw-storybook-addon';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
 import { handlers as mutationHandlers } from '../src/mocks/mutations/handlers';
 import { handlers as queryHandlers } from '../src/mocks/queries/handlers';
 import { dateFns } from '../src/shared/dateFns';
 import { Provider } from '../src/storybook/Provider';
 
-msw.initialize();
+initialize();
 
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    nextjs: {
+      appDirectory: true,
+    },
+    msw: {
+      handlers: [...queryHandlers, ...mutationHandlers],
     },
   },
-  nextjs: {
-    appDirectory: true,
-  },
-  msw: {
-    handlers: [...queryHandlers, ...mutationHandlers],
-  },
+
+  decorators: [
+    mswDecorator,
+    (Story, { parameters }) => {
+      const date = parameters.mockDate ?? '2022-11-29T09:16:39+09:00';
+
+      mockdate.set(date);
+
+      const mockedDate = dateFns.format(new Date(date), 'HH:mm:ss dd/MM/yy');
+      console.log('[Mocked date]: ', mockedDate);
+
+      return <Story />;
+    },
+    (Story) => (
+      <Provider>
+        <Story />
+      </Provider>
+    ),
+  ],
 };
 
-export const mockDateDecorator = (Story, { parameters }) => {
-  const date = parameters.mockDate ?? '2022-11-29T09:16:39+09:00';
-
-  mockdate.set(date);
-
-  const mockedDate = dateFns.format(new Date(date), 'HH:mm:ss dd/MM/yy');
-  console.log('[Mocked date]: ', mockedDate);
-
-  return <Story />;
-};
-
-export const decorators = [
-  msw.mswDecorator,
-  mockDateDecorator,
-  (Story) => (
-    <Provider>
-      <Story />
-    </Provider>
-  ),
-];
+export default preview;
